c82aceecfc9edc95611e11670a46d6896b89a8cc
balaga raghuram <balagaraghuram1@gmail.com>
2026-03-10 10:21:00 +0000

    chore: update src/utils/helpers.py v48

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -75,3 +75,4 @@ def truncate_text(text: str, max_length: int = 100, suffix: str = "...") -> str:
         return text
     return text[: max_length - len(suffix)] + suffix
 # v18 - updated 2026-06-11
+# v48 - updated 2026-06-11
